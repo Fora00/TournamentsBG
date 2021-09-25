307723202c4b143cdb7975ed3c952f0b960b4c0a
Francesco Elia Foradori <francesco.elia.foradori@gmail.com>
2021-09-25 14:08:50 +0200

    add: protofire

---
+++ src/Firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "bg-tournaments.firebaseapp.com",
+  projectId: "bg-tournaments",
+  storageBucket: "bg-tournaments.appspot.com",
+  messagingSenderId: "428740427133",
+  appId: "1:428740427133:web:2347eb8da2904e2088e014",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
