cc91bb17ef4250abe6ec17adaf0e5a6def11ef72
Francesco Elia Foradori <francesco.elia.foradori@gmail.com>
2021-10-29 11:39:47 +0200

    refactor: add ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier', '@vue/typescript'],
+
   parserOptions: {
-    parser: "babel-eslint",
+    parser: '@typescript-eslint/parser',
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
